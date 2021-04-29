4eac4aa4ae6dc03815973aa59ba6451d76da7f43
Anti <benyeiantal@gmail.com>
2021-04-29 12:35:20 +0200

    d

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/MilitaryUnit.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/MilitaryUnit.java
@@ -1,3 +1,30 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public abstract class MilitaryUnit {}
+public abstract class MilitaryUnit {
+
+    protected int hitPoints;
+    protected int damage;
+    protected boolean armor;
+
+    public MilitaryUnit(int hitPoints, int damage, boolean armor) {
+        this.hitPoints = hitPoints;
+        this.damage = damage;
+        this.armor = armor;
+    }
+
+    public int doDamage() {
+        return this.damage;
+    }
+
+    public void sufferDamage(int damage) {
+        if (this.armor) {
+            this.hitPoints -= damage / 2;
+        } else {
+            this.hitPoints -= damage;
+        }
+    }
+
+    public int getHitPoints() {
+        return this.hitPoints;
+    }
+}
